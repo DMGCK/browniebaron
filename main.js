3bde82a91a7a338a2822426c3895aaf059a7f023
cameron <cameron.kueneman@gmail.com>
2023-08-03 02:21:05 -0600

    contact form done

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
-import axios from "axios"
-
-const ax = axios.create({
-  baseURL: "https://api.emailjs.com/api/v1.1",
-  timeout: 1000
-})
 
 
-async function init() {
-  const res = await ax.get("/history", {
-    user_id: "",
-    accessToken	: ""
-  })
+const form = document.querySelector("form")
+console.log(form)
+
+form.addEventListener("submit", submit, false)
+
+const cred = {
+  user_id: "-OlpHHuvasLZhr-SE",
+  service_id: "3b_contact",
+  template_id: "3b_contact_form",
+  template_params: {
+    from_name: "",
+    reply_to: "",
+    message: ""
+  }
 }
 
-init()
+
+async function submit(e) {
+  const userInput = [];
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length != 0) {
+      userInput.push(form.elements[i].value)
+    }
+  }
+  e.preventDefault();
+  e.stopPropagation();
+  
+  console.log(userInput)
+  cred.template_params.from_name = userInput[0]
+  cred.template_params.reply_to = userInput[1]
+  cred.template_params.message = userInput[2]
+
+try {
+  emailjs.send(cred.service_id,cred.template_id, cred.template_params)
+	.then(function(response) {
+	   console.log('SUCCESS!', response.status, response.text);
+	}, function(err) {
+	   console.log('FAILED...', err);
+	});
+} catch (error) {
+  console.error(error) 
+}
+}
